fb4d0c455ae4744f6e3b9b176c6c2562dd2bde9e
KRISHNA <keshuyadav2010@gmail.com>
2019-01-12 21:00:22 +0530

    Add files via upload

---
+++ DHT_mail/DHT_mail.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+//#include <Adafruit_MQTT_FONA.h>
+
+#include <Adafruit_Sensor.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include "DHT.h"
+#define DHTTYPE DHT11
+
+#define dpin D4
+DHT dht(dpin, DHTTYPE);
+
+void setup() {
+
+  dht.begin();
+  Serial.begin(115200);
+  Serial.println("\nHUMIDTY and TEMPERATURE/n/n");
+  delay(700);
+}
+
+void loop() {
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Current Humidity = ");
+  Serial.print(h);
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.println("`C");
+
+  if(isnan(h)||isnan(t))
+  {
+    return ;
+  }
+  
+
+  delay(30000);
+
+}
